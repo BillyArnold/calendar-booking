c95ca56825e3d7da6520f6b18bf712afe7c55258
Billy Arnold <billyarnoldd@gmail.com>
2022-10-27 11:18:19 +0100

    Adding my calendar dashboard page, setting up calendar db schema

--- src/pages/my-calendars.tsx
+++ src/pages/my-calendars.tsx
@@ -10,36 +10,27 @@ const Home: NextPage = () => {
     <>
       <div className="bg-gray-900 text-white">
         <Head>
-          <title>Calendar Share</title>
+          <title>My Calendars</title>
           <meta name="description" content="Share calendar meetings easily" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
         <main className="mx-auto w-3/4">
           <div className="flex min-h-screen w-full flex-col justify-center ">
-            <h1 className="mb-4 text-5xl font-bold">Welcome Back!</h1>
+            <h1 className="mb-6 text-5xl font-bold">My Calendars</h1>
 
             {session ? (
               <div>
-                <p className="mb-6">Hi {session.user?.name}</p>
-
-                <Link href="/my-calendars">
-                  <div className="mb-5 w-56 max-w-full cursor-pointer bg-indigo-600 py-2 px-10 text-center font-semibold text-slate-200 shadow-lg">
-                    My Calendars
-                  </div>
-                </Link>
-
                 <button onClick={() => signOut()}>Logout</button>
               </div>
             ) : (
-              <div className="">
-                <p className="mb-6">Log in to access your calendars below:</p>
-                <button
-                  className="mb-5 max-w-full bg-indigo-600 py-2 px-10 font-semibold text-slate-200 shadow-lg"
-                  onClick={() => signIn("discord")}
-                >
-                  Login with Discord
-                </button>
+              <div>
+                <p className="mb-6">
+                  Please Login to view calendars{" "}
+                  <Link href="/">
+                    <a className="text-blue-400">here</a>
+                  </Link>
+                </p>
               </div>
             )}
           </div>
